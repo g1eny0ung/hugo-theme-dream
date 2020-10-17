16b7495f70bca58762dc641a8908bf0280e8959f
Yue Yang <g1enyy0ung@gmail.com>
2020-10-17 19:20:39 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -10,13 +10,8 @@ const StylesOutput = './static/css'
 const JsEntry = './src/js/**/*.js'
 const JsOutput = './static/js'
 
-const sassDev = () =>
-  src(StylesEntry)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(dest(StylesOutput))
-
+const sassDev = () => src(StylesEntry).pipe(sass().on('error', sass.logError)).pipe(dest(StylesOutput))
 const sassWatch = () => watch(StylesEntry, sassDev)
-
 const sassProd = () =>
   src(StylesEntry)
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
@@ -26,23 +21,20 @@ const jsDev = () =>
   src(JsEntry)
     .pipe(
       babel({
-        presets: ['@babel/preset-env']
+        presets: ['@babel/preset-env'],
       })
     )
     .pipe(dest(JsOutput))
-
 const jsWatch = () => watch(JsEntry, jsDev)
-
 const jsProd = () =>
   src(JsEntry)
     .pipe(
       babel({
-        presets: ['@babel/preset-env']
+        presets: ['@babel/preset-env'],
       })
     )
     .pipe(uglify())
     .pipe(dest(JsOutput))
 
 exports.watch = parallel(sassWatch, jsWatch)
-
 exports.prod = parallel(sassProd, jsProd)
