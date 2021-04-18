dc1120227f6333dcc10d8b59855a7a3b7031a9a1
Yue Yang <g1enyy0ung@gmail.com>
2021-04-19 00:48:22 +0800

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -10,22 +10,12 @@ const StylesOutput = './static/css'
 const JsEntry = './src/js/**/*.js'
 const JsOutput = './static/js'
 
-const sassDev = () => src(StylesEntry).pipe(sass().on('error', sass.logError)).pipe(dest(StylesOutput))
-const sassWatch = () => watch(StylesEntry, sassDev)
 const sassProd = () =>
   src(StylesEntry)
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(dest(StylesOutput))
+const sassWatch = () => watch(sassProd, sassDev)
 
-const jsDev = () =>
-  src(JsEntry)
-    .pipe(
-      babel({
-        presets: ['@babel/preset-env'],
-      })
-    )
-    .pipe(dest(JsOutput))
-const jsWatch = () => watch(JsEntry, jsDev)
 const jsProd = () =>
   src(JsEntry)
     .pipe(
@@ -35,6 +25,7 @@ const jsProd = () =>
     )
     .pipe(uglify())
     .pipe(dest(JsOutput))
+const jsWatch = () => watch(JsEntry, jsProd)
 
 exports.watch = parallel(sassWatch, jsWatch)
 exports.prod = parallel(sassProd, jsProd)
