f85cd89093608f31104cc10f325436aac28ebe37
Yue Yang <g1enyy0ung@gmail.com>
2025-01-18 14:41:01 +0800

    docs: use hugo-theme-dream.g1en.site

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -7,10 +7,10 @@ const withNextra = nextra({
 
 export default withNextra({
   output: 'export',
-  ...(process.env.NODE_ENV === 'production' &&
-    process.env.IN_NETLIFY !== 'true' && {
-      basePath: '/hugo-theme-dream',
-    }),
+  // ...(process.env.NODE_ENV === 'production' &&
+  //   process.env.IN_NETLIFY !== 'true' && {
+  //     basePath: '/hugo-theme-dream',
+  //   }),
   images: {
     unoptimized: true,
   },
