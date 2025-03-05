516a8ddac1d00d9a24fa2decebcb832366df67b2
Yue Yang <g1enyy0ung@gmail.com>
2025-03-05 13:29:30 +0800

    chore: remove <Callout /> imports

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -4,10 +4,6 @@ const withNextra = nextra({})
 
 export default withNextra({
   output: 'export',
-  // ...(process.env.NODE_ENV === 'production' &&
-  //   process.env.IN_NETLIFY !== 'true' && {
-  //     basePath: '/hugo-theme-dream',
-  //   }),
   images: {
     unoptimized: true,
   },
