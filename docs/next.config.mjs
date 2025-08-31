da94b9b86a85e9345b95a47b54c378f664e56f94
Yue Yang <g1enyy0ung@gmail.com>
2025-08-31 10:35:38 +0800

    docs: upgrade to nextra v4 (#372)

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,20 +1,14 @@
 import nextra from 'nextra'
+import path from 'path'
 
-const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
-})
+const withNextra = nextra({})
 
 export default withNextra({
+  turbopack: {
+    root: path.resolve('./'),
+  },
   output: 'export',
-  // ...(process.env.NODE_ENV === 'production' &&
-  //   process.env.IN_NETLIFY !== 'true' && {
-  //     basePath: '/hugo-theme-dream',
-  //   }),
   images: {
     unoptimized: true,
   },
 })
-
-// If you have other Next.js configurations, you can pass them as the parameter:
-// export default withNextra({ /* other next.js config */ })
